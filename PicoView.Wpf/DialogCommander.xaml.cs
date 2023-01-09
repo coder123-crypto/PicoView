91d0d001851dcc11dc581e782a2e8058116e6cce
lereminbiz@gmail.com <leremin>
2023-01-09 10:53:31 +0300

    Добавлена возможность менять видимость кнопок

--- PicoView.Wpf/DialogCommander.xaml.cs
+++ PicoView.Wpf/DialogCommander.xaml.cs
@@ -18,6 +18,33 @@ public partial class DialogCommander
         set => SetValue(ValidProperty, value);
     }
 
+    public static readonly DependencyProperty ButtonsProperty = DependencyProperty.Register(
+        nameof(Buttons),
+        typeof(DialogCommanderButtons),
+        typeof(DialogCommander),
+        new FrameworkPropertyMetadata(DialogCommanderButtons.Ok | DialogCommanderButtons.Cancel, FrameworkPropertyMetadataOptions.AffectsArrange, ButtonsChanged)
+    );
+
+    private static void ButtonsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is DialogCommander commander)
+        {
+            commander.ButtonsChanged();
+        }
+    }
+
+    private void ButtonsChanged()
+    {
+        OkButton.Visibility = Buttons.HasFlag(DialogCommanderButtons.Ok) ? Visibility.Visible : Visibility.Collapsed;
+        CancelButton.Visibility = Buttons.HasFlag(DialogCommanderButtons.Cancel) ? Visibility.Visible : Visibility.Collapsed;
+    }
+
+    public DialogCommanderButtons Buttons
+    {
+        get => (DialogCommanderButtons) GetValue(ButtonsProperty);
+        set => SetValue(ButtonsProperty, value);
+    }
+
     public DialogCommander()
     {
         InitializeComponent();
---
+++ PicoView.Wpf/DialogCommanderButtons.cs
@@ -0,0 +1,8 @@
+﻿namespace PicoView.Wpf;
+
+[Flags]
+public enum DialogCommanderButtons
+{
+    Ok = 0x1,
+    Cancel = 0x2
+}
